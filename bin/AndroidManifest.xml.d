b013c30b17d9fbc436064928a3381524ec6c1801
Andrey Pereverzin <andreypereverzin@Andreys-MacBook-Pro.local>
2012-12-03 07:10:13 +0000

    deleted bin

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/private/etc/Work/staccato/bin/AndroidManifest.xml : \
-/private/etc/Work/staccato/AndroidManifest.xml \
--- bin/staccato-debug-unaligned.apk.d
+++
@@ -1,9 +0,0 @@
-/private/etc/Work/staccato/bin/staccato-debug-unaligned.apk : \
-/private/etc/Work/staccato/bin/staccato.ap_ \
-/private/etc/Work/staccato/bin/classes.dex \
-/private/etc/Work/staccato/libs/json-simple-1.1.jar \
-/Users/andreypereverzin/android-sdks/tools/support/annotations.jar \
-/private/etc/Work/staccato/libs/staccato-model-1.0.1.jar \
-/private/etc/Work/staccato/libs/additionnal.jar \
-/private/etc/Work/staccato/libs/mail.jar \
-/private/etc/Work/staccato/libs/activation.jar \
